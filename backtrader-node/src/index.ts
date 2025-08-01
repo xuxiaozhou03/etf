08281d32cd0579b0c04f8f66ed3b7fed3a735543
许晓周 <759811542@qq.com>
2025-08-01 16:28:24 +0800

    chore

--- backtrader-node/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { getEtfs } from "./data/getEtfs";
-
-const main = async () => {
-  const etfs = await getEtfs();
-  console.log("ETF 列表:", etfs);
-};
-
-main();
--- backtrader-node/src/risk/types.ts
+++
@@ -1,6 +0,0 @@
-// 风控模块类型定义
-import { Order, Position } from "../trade/types";
-export interface RiskRule {
-  check(order: Order, positions: Position[]): boolean;
-  name: string;
-}
